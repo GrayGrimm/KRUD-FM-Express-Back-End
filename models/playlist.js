cb29844eb43a426adf0f5acafb80423b69947d58
Shaina G <shainagtherapy@gmail.com>
2025-08-22 09:20:59 -0700

    Create playlistSchema: partial

---
+++ models/playlist.js
@@ -0,0 +1,24 @@
+const playlistSchema = new mongoose.Schema({
+        playlist: {
+            type: String,
+            required: true,
+        },
+        genre: {
+            type: String,
+            required: true,
+        },
+        station: {
+            type: String,
+            required: true,
+            enum: [],
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        songsSchema: {ref: songId}
+});
+
+const Playlist = mongoose.model('Playlist', playlistSchema);
+
+modules.exports = Playlist;
