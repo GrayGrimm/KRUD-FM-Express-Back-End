54946f33b67bb74c8ff97f634467a8b416c312aa
GenioZx <eli.rodri14@email.com>
2025-08-25 11:54:42 -0400

    delete route created

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -9,7 +9,7 @@ router.post('/', verifyToken, async (req, res) => {
     const playlist = await Playlist.create(req.body);
     playlist._doc.author = req.user;
     res.status(201).json(playlist);
-  } catch (err) {
+  } catch(err) {
     res.status(500).json({ err: err.message })
   }
 })
@@ -22,7 +22,7 @@ router.get("/", verifyToken, async (req, res) => {
       .populate("genre")
       .populate("station")
     res.status(200).json(playlists);
-  } catch (err) {
+  } catch(err) {
     res.status(500).json({ err: err.message })
   }
 });
@@ -32,7 +32,7 @@ router.get('/:playlistId', verifyToken, async (req, res) => {
     const playlist = await Playlist.findById(req.params.playlistId)
       .populate("author")
     res.status(200).json(playlist);
-  } catch (err) {
+  } catch(err) {
     res.status(500).json({ err: err.message })
   }
 })
@@ -40,15 +40,28 @@ router.get('/:playlistId', verifyToken, async (req, res) => {
 router.put('/:playlistId', verifyToken, async (req, res) => {
   try {
     const playlist = await Playlist.findById(req.params.playlistId)
-    if ( !playlist.author.equals(req.user._id) ) {
+    if ( !playlist.author.equals(req.user._id)) {
       return res.status(403).send("You are not allowed!")
     }
     const updatedPlaylist = await Playlist.findByIdAndUpdate(req.params.playlistId, req.body, { new: true });
     updatedPlaylist._doc.author = req.user;
     res.status(200).json(updatedPlaylist)
-  } catch (err) {
+  } catch(err) {
     res.status(500).json({ err: err.message })
   }
 });
+
+router.delete('/:playlistId', verifyToken, async (req, res) => {
+  try {
+    const playlist = await Playlist.findById(req.params.playlistId)
+    if ( !playlist.author.equals(req.user._id)) {
+      return res.status(403).send("You are not allowed!")
+    } 
+    const deletedPlaylist = await Playlist.findByIdAndDelete(req.params.playlistId);
+    res.status(200).json(deletedPlaylist)
+  } catch(err){
+    res.status(500).json({ err: err.message })
+  }
+})
 
 module.exports = router;
